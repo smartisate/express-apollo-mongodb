27076de089902f7532e50e0a7a874ff68bd8e01d
Smartisate Develop <smartisate.develop@gmail.com>
2023-07-06 18:42:30 -0400

    test apollo-graphql basic from http://localhost:3000/graphql/ -> read readme.md

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+
+const {typeDefs} = require('./typeDefs');
+const {resolvers} = require('./resolvers');
+
+
+const app = express();
+
+module.exports = app;
+
+async function start() {
+
+    const apolloServer = new ApolloServer({
+        typeDefs:typeDefs,
+        resolvers:resolvers,
+    })
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({app:app});
+
+    app.listen(3000, () => {
+        console.log('Server on port', 3000);
+    });
+};
+
+start();
---
+++ readme.md
@@ -0,0 +1,20 @@
+npm init -y
+npm i express apollo-server-express dotenv graphql mongoose
+node index.js
+
+---------------------
+Check http://localhost:3000/grapql/ -> Query your Server
+
+in -- operation text area -- ->
+        
+        query{
+          hello
+        }
+
+-- click Run button --
+
+Check in -- Response  text area--- to view results
+
+
+
+
